156703a15a0bc0f834532ac7c90b4cedcbdaba62
Bruna Radeke <brunaradeke@hotmail.com>
2020-04-30 10:08:32 +0100

    6- Load the "Course" component as a nested component of "Courses"

--- src/containers/Course/Course.js
+++ src/containers/Course/Course.js
@@ -5,14 +5,25 @@ class Course extends Component {
         courseTitle: ''
     }
     componentDidMount() {
+        this.parseQueryParams();
+    }
+
+    componentDidUpdate() {
+        this.parseQueryParams();
+    }
+
+    parseQueryParams() {
         console.log(this.props);
         const query = new URLSearchParams(this.props.location.search);
         //console.log(query);
         //Gettting the param index 1 does means the title
         for (let param of query.entries()) {
-            this.setState({ courseTitle: param[1] });
-        };
+            if (this.state.courseTitle !== param[1]) {
+                this.setState({ courseTitle: param[1] });
+            }
+        }
     }
+
     render() {
         return (
             <div>
